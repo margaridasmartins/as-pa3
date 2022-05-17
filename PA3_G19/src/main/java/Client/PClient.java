cac59bd87c9ca7bf951b74b8f682ec8bb5b30a86
Margarida <margarida.martins@ua.pt>
2022-05-17 12:01:45 +0100

    project structure

---
+++ PA3_G19/src/main/java/Client/PClient.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Client;
+
+/**
+ *
+ * Client process class
+ */
+public class PClient {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ PA3_G19/src/main/java/Monitor/PMonitor.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Monitor;
+
+/**
+ *
+ * Monitor process class
+ */
+public class PMonitor {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
