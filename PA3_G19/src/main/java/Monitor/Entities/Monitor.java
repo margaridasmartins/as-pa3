ee82f8f92c96fb91bbdab87e89864184d64c0c62
leand12 <leandrosilva12@ua.pt>
2022-06-21 13:40:51 +0100

    monitor done ig

--- PA3_G19/src/main/java/Monitor/Entities/Monitor.java
+++ PA3_G19/src/main/java/Monitor/Entities/Monitor.java
@@ -11,6 +11,7 @@ import Utils.ServerStatusMessage;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -18,24 +19,24 @@ import java.util.concurrent.locks.ReentrantLock;
  * @author leand
  */
 public class Monitor {
-    
+
     /* Status of every server by ID */
     private final HashMap<Integer, ServerRequestsInfo> serversInfo;
-    
+
     /* List of requests of every server by ID */
     private final HashMap<Integer, HashMap<Integer, RequestMessage>> serversRequests;
-    
+
     /* Requests information of every client by ID */
     private final HashMap<Integer, ClientRequestsInfo> clientsInfo;
-    
+
     /* List of all requests handled by the Load Balancer */
     private final List<RequestMessage> loadBalancerRequests;
-    
+
     /* Whether or not the primary Load Balancer is working */
     private boolean hasPrimaryLB;
-    
+
     private final ReentrantLock rl;
-    
+
     public Monitor(GUI gui) {
         serversInfo = new HashMap<>();
         serversRequests = new HashMap<>();
@@ -43,80 +44,141 @@ public class Monitor {
         loadBalancerRequests = new ArrayList<>();
         rl = new ReentrantLock();
     }
-    
+
     public boolean hasPrimaryLB() {
         return hasPrimaryLB;
     }
-    
+
+    /**
+     * Lock the monitor status and update it according to the forwarded request.
+     *
+     * @param request the forwarded request
+     */
     public void forwardingRequest(RequestMessage request) {
         int clientID = request.clientID();
         int serverID = request.serverID();
         int requestID = request.requestID();
-        
-        serversInfo.getOrDefault(serverID, new ServerRequestsInfo())
-                .receiveRequest(request.nIterations());
-        serversRequests.getOrDefault(serverID, new HashMap<>())
-                .put(requestID, request);
-        clientsInfo.getOrDefault(clientID, new ClientRequestsInfo())
-                .forwardingRequest();
-        loadBalancerRequests.add(request);
+
+        try {
+            rl.lock();
+            serversInfo.getOrDefault(serverID, new ServerRequestsInfo())
+                    .receiveRequest(request.nIterations());
+            serversRequests.getOrDefault(serverID, new HashMap<>())
+                    .put(requestID, request);
+            clientsInfo.getOrDefault(clientID, new ClientRequestsInfo())
+                    .forwardingRequest();
+            loadBalancerRequests.add(request);
+        } finally {
+            rl.unlock();
+        }
     }
-    
+
+    /**
+     * Lock the monitor status and update it according to the message.
+     *
+     * @param msg the message containing the request's client ID
+     */
     public void processingRequest(Message msg) {
         int cliendID = msg.port();
-        
-        clientsInfo.getOrDefault(cliendID, new ClientRequestsInfo())
+
+        try {
+            rl.lock();
+            clientsInfo.getOrDefault(cliendID, new ClientRequestsInfo())
                 .forwardingRequest();
+        } finally {
+            rl.unlock();
+        }
     }
-    
+
+    /**
+     * Lock the monitor status and update it according to the replied message.
+     *
+     * @param request the replied message
+     */
     public void replyingRequest(RequestMessage request) {
         int cliendID = request.port();
         int serverID = request.serverID();
         int requestID = request.requestID();
-        
-        serversInfo.getOrDefault(serverID, new ServerRequestsInfo())
-                .completeRequest(request.nIterations());
-        clientsInfo.getOrDefault(cliendID, new ClientRequestsInfo())
-                .replyingRequest();
-        serversRequests.getOrDefault(serverID, new HashMap<>())
-                .remove(requestID);
+
+        try {
+            rl.lock();
+            serversInfo.getOrDefault(serverID, new ServerRequestsInfo())
+                    .completeRequest(request.nIterations());
+            clientsInfo.getOrDefault(cliendID, new ClientRequestsInfo())
+                    .replyingRequest();
+            serversRequests.getOrDefault(serverID, new HashMap<>())
+                    .remove(requestID);
+        } finally {
+            rl.unlock();
+        }
     }
-    
+
+    /**
+     * Build the status message list of all requests for every server.
+     *
+     * @return the status message list
+     */
+    public List<ServerStatusMessage> getServersStatus() {
+        List<ServerStatusMessage> serversStatus = new ArrayList<>();
+
+        try {
+            rl.lock();
+            for (Map.Entry<Integer, ServerRequestsInfo> entry : serversInfo.entrySet()) {
+                int serverID = entry.getKey();
+                int nRequests = entry.getValue().nRequests;
+                int totalIterations = entry.getValue().totalIterations;
+                serversStatus.add(new ServerStatusMessage(serverID, nRequests, totalIterations));
+            }
+            return serversStatus;
+            
+        } finally {
+            rl.unlock();
+        }
+    }
+
+    /**
+     * Representation of the Client's requests information
+     */
     class ClientRequestsInfo {
+
         private int pending = 0;
         private int beingProcessed = 0;
         private int rejected = 0;
         private int processed = 0;
-        
+
         void forwardingRequest() {
             pending++;
         }
-        
+
         void processingRequest() {
             pending--;
             beingProcessed++;
         }
-        
+
         void rejectingRequest() {
             beingProcessed--;
             rejected++;
         }
-        
+
         void replyingRequest() {
             beingProcessed--;
             processed++;
         }
-    } 
-    
+    }
+
+    /**
+     * Representation of the Server's requests information
+     */
     class ServerRequestsInfo {
+
         int nRequests = 0;
         int totalIterations = 0;
-        
+
         void receiveRequest(int iterations) {
             nRequests++;
             totalIterations += iterations;
         }
-        
+
         void completeRequest(int iterations) {
             nRequests--;
             totalIterations -= iterations;
