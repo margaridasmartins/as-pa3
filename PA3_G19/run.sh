17713a91709b79497d478aa077e668ed9f0b77bb
leand12 <leandrosilva12@ua.pt>
2022-06-22 02:34:47 +0100

    new script to run without maven

--- PA3_G19/run.sh
+++ PA3_G19/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-mvn package
-gnome-terminal --tab --title="Client" -- java -cp target/PA3_G19-1.0-SNAPSHOT.jar Client.PClient
-gnome-terminal --tab --title="Monitor" -- java -cp target/PA3_G19-1.0-SNAPSHOT.jar Monitor.PMonitor
-gnome-terminal --tab --title="LoadBalancer" -- java -cp target/PA3_G19-1.0-SNAPSHOT.jar LB.PLoadBalancer
-gnome-terminal --tab --title="Server" -- java -cp target/PA3_G19-1.0-SNAPSHOT.jar Server.PServer
+javac -d target $(find . | grep "\.java$")
 
+gnome-terminal --tab --title="Client" -- java -cp target Client.PClient
+gnome-terminal --tab --title="Monitor" -- java -cp target Monitor.PMonitor
+gnome-terminal --tab --title="LoadBalancer" -- java -cp target LB.PLoadBalancer
+gnome-terminal --tab --title="Server" -- java -cp target Server.PServer
